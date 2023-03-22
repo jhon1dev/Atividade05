fdc9dee4f351fd13c81951dcc0d18c04bfcfc5ad
JHONATAN ERIK DOS SANTOS SILVA <2021325119@IFAL-MACEIO.INTRANET>
2023-03-22 17:56:05 -0300

    Upload of project files

---
+++ script.js
@@ -0,0 +1,8 @@
+var form1 = document.getElementById("formulario-matricula");
+var form2 = document.getElementById("infos-adicionais")
+
+function confirmar(){
+    form1.style.left = "-400px";
+    form1.style.display = "none";
+    form2.style.left = "50px";
+}
