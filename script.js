d988e589adbc65aacbdcc3cdad135aa5f01cd9c4
JHONATAN ERIK DOS SANTOS SILVA <2021325119@IFAL-MACEIO.INTRANET>
2023-03-23 09:07:06 -0300

    Implemented clear button to the project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var form1 = document.getElementById("formulario-matricula");
 var form2 = document.getElementById("infos-adicionais");
+var clearBtn = document.getElementById("clear-btn");
 var telefoneInput = document.getElementById("tel");
 
 const dddsBrasil = ["11", "12", "13", "14", "15", "16", "17", "18", "19", "21", "22", "24", "27", "28", "31", "32", "33", "34", "35", "37", "38", "41", "42", "43", "44", "45", "46", "47", "48", "49", "51", "53", "54", "55", "61", "62", "63", "64", "65", "66", "67", "68", "69", "71", "73", "74", "75", "77", "79", "81", "82", "83", "84", "85", "86", "87", "88", "89", "91", "92", "93", "94", "95", "96", "97", "98", "99"];
@@ -16,10 +17,8 @@ telefoneInput.addEventListener("input", function(event) {
     telefoneInput.setCustomValidity("");
   }
   
-  // Formata o número de telefone
   var telefoneFormatado = '(' + ddd + ') ' + telefone.slice(2, 3) + ' ' + telefone.slice(3, 7) + '-' + telefone.slice(7, 11);
   
-  // Atualiza o valor do input
   telefoneInput.value = telefoneFormatado;
 });
 
@@ -31,4 +30,11 @@ function confirmar(){
     else{
         alert("por favor, preencha todos os campos corretamente!");
     }
+}
+
+function clearArea(){
+  var inputs = document.getElementsByClassName("input-field");
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].value = "";
+  }
 }
