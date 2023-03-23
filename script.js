1a66689b6254eb83cd8230c8051a73bb8d9df262
JHONATAN ERIK DOS SANTOS SILVA <2021325119@IFAL-MACEIO.INTRANET>
2023-03-23 16:38:17 -0300

    Updated some changes.

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-var form1 = document.getElementById("formulario-matricula");
-var clearBtn = document.getElementById("clear-btn");
 var telefoneInput = document.getElementById("tel");
 var emailInput = document.getElementById("email");
 var boxInputs = document.getElementsByClassName("box");
 
 const dddsBrasil = ["11", "12", "13", "14", "15", "16", "17", "18", "19", "21", "22", "24", "27", "28", "31", "32", "33", "34", "35", "37", "38", "41", "42", "43", "44", "45", "46", "47", "48", "49", "51", "53", "54", "55", "61", "62", "63", "64", "65", "66", "67", "68", "69", "71", "73", "74", "75", "77", "79", "81", "82", "83", "84", "85", "86", "87", "88", "89", "91", "92", "93", "94", "95", "96", "97", "98", "99"];
 
-telefoneInput.addEventListener("input", function(event) { // Ativa com um input - Telefone
+telefoneInput.addEventListener("input", function(event) {
 
   var telefone = telefoneInput.value.replace(/\D/g, '');
   
@@ -22,7 +20,7 @@ telefoneInput.addEventListener("input", function(event) { // Ativa com um input 
   telefoneInput.value = telefoneFormatado;
 });
 
-emailInput.addEventListener("input", function(event){ // Ativa com um input - Email
+emailInput.addEventListener("input", function(event){
   if(!(emailInput.value.includes(".") && emailInput.value.includes("@"))){
     emailInput.setCustomValidity("Por favor, insira um email válido.");
   } 
@@ -31,7 +29,7 @@ emailInput.addEventListener("input", function(event){ // Ativa com um input - Em
   }
 });
 
-function confirmar(){
+function validCheckbox(){
   var boxContador = 0;
   for (var i = 0; i <= boxInputs.length; i++) {
     var element = boxInputs[i];
