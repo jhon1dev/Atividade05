224f027db466bd7a6e52a9172e89d24731d1a5cc
jhondev <125105950+jhon1dev@users.noreply.github.com>
2023-03-24 17:56:47 -0300

    Fixed code issues.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var telefoneInput = document.getElementById("tel");
 var emailInput = document.getElementById("email");
-var boxInputs = document.getElementsByClassName("box");
 
 const dddsBrasil = ["11", "12", "13", "14", "15", "16", "17", "18", "19", "21", "22", "24", "27", "28", "31", "32", "33", "34", "35", "37", "38", "41", "42", "43", "44", "45", "46", "47", "48", "49", "51", "53", "54", "55", "61", "62", "63", "64", "65", "66", "67", "68", "69", "71", "73", "74", "75", "77", "79", "81", "82", "83", "84", "85", "86", "87", "88", "89", "91", "92", "93", "94", "95", "96", "97", "98", "99"];
 
@@ -29,16 +28,10 @@ emailInput.addEventListener("input", function(event){
   }
 });
 
-function validCheckbox(){
-  var boxContador = 0;
-  for (var i = 0; i <= boxInputs.length; i++) {
-  
-    if (boxInputs[i].checked) {
-      boxContador++;
-    }
-    if (boxContador > 3) {
-      boxInputs[i].checked = false;
-      break;
-    }
+function validCheckbox(event){
+  var boxInputs = document.querySelectorAll('input[type=checkbox]:checked'); // devolve um array de inputs do tipo checkbox com todas as checkbox que forem selecionadas.
+
+  if(boxInputs.length > 3){
+    event.target.checked = false;
   }
 }
